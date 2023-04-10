6e25efd4e7ab5ba939938e87150ced25fcfe6fc9
Zeke <erb1995@gmail.com>
2023-04-10 16:02:40 -0300

    Update SpotifySongReleaseDateStrategy.kt
    Fixed some implementations.

--- app/src/main/java/ayds/newyork/songinfo/home/model/entities/SpotifySongReleaseDateStrategy.kt
+++ app/src/main/java/ayds/newyork/songinfo/home/model/entities/SpotifySongReleaseDateStrategy.kt
@@ -1,5 +1,7 @@
 package ayds.newyork.songinfo.home.model.entities
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import java.text.SimpleDateFormat
 import java.util.Calendar
 import java.util.Locale
@@ -9,15 +11,20 @@ interface SpotifySongReleaseDateStrategy {
 }
 
 object SpotifySongReleaseDateByYearStrategy: SpotifySongReleaseDateStrategy {
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun printReleaseDate(releaseDate: String): String {
-        return releaseDate.split("-").first()
+        val year = releaseDate.split("-").first().toInt()
+        return if(Calendar.isGregorianLeapYear(year)){
+            "$year (leap year)"
+        } else {
+            "$year (not a leap year)"
+        }
     }
 }
 
 object SpotifySongReleaseDateByMonthStrategy: SpotifySongReleaseDateStrategy {
-    @RequiresApi(Build.VERSION_CODES.N)
     override fun printReleaseDate(releaseDate: String): String {
-        val monthDate = SimpleDateFormat("MMMM, YYYY", Locale("en"))
+        val monthDate = SimpleDateFormat("MMMMM, yyyy", Locale("en"))
         val info = releaseDate.split("-")
         return monthDate.format(Calendar.getInstance().set(info[0].toInt(), info[1].toInt(), info[2].toInt()))
     }
