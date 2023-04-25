c6c50b17c238ad00d80f78d54e479ae53ae90ae8
GonzaloMontenegro <42841254+GonzaloMonte@users.noreply.github.com>
2023-04-25 17:43:12 -0300

    pasaje de codigo java a kotlin

--- app/src/main/java/ayds/newyork/songinfo/moredetails/fulllogic/OtherInfoWindow.kt
+++ app/src/main/java/ayds/newyork/songinfo/moredetails/fulllogic/OtherInfoWindow.kt
@@ -1,204 +1,179 @@
-package ayds.newyork.songinfo.moredetails.fulllogic;
+package ayds.newyork.songinfo.moredetails.fulllogic
 
-import android.content.Intent;
-import android.net.Uri;
-import android.os.Bundle;
-import android.text.Html;
-import android.util.Log;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.TextView;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import ayds.newyork.songinfo.R;
-import com.google.gson.Gson;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.squareup.picasso.Picasso;
-import java.io.IOException;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.scalars.ScalarsConverterFactory;
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.text.Html
+import android.util.Log
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import ayds.newyork.songinfo.R
+import com.google.gson.Gson
+import com.google.gson.JsonElement
+import com.google.gson.JsonObject
+import com.squareup.picasso.Picasso
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import java.io.IOException
+import java.util.*
 
-public class OtherInfoWindow extends AppCompatActivity {
-  public final static String ARTIST_NAME_EXTRA = "artistName";
-  private TextView ArtistInfoView;
-  private DataBase dataBase;
-  public OtherInfoWindow (){
-    dataBase = null;
-  }
+class OtherInfoWindow : AppCompatActivity() {
+    private var ArtistInfoView: TextView? = null
+    private var dataBase: DataBase? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_other_info)
+        ArtistInfoView = findViewById(R.id.textPane2)
+        open(intent.getStringExtra("artistName"))
+    }
 
-  @Override
-  protected void onCreate(@Nullable Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-
-    setContentView(R.layout.activity_other_info);
-
-    ArtistInfoView = findViewById(R.id.textPane2);
-
-    open(getIntent().getStringExtra("artistName"));
-  }
-
-  private Retrofit getRetrofit(){
-    Retrofit retrofit = new Retrofit.Builder()
+    private val retrofit: Retrofit
+        private get() = Retrofit.Builder()
             .baseUrl("https://api.nytimes.com/svc/search/v2/")
             .addConverterFactory(ScalarsConverterFactory.create())
-            .build();
-    return retrofit;
-  }
+            .build()
 
-  private NYTimesAPI createNYTimesApi(){
-    Retrofit retrofit = getRetrofit();
-    NYTimesAPI nyTimesApi = retrofit.create(NYTimesAPI.class);
-    return nyTimesApi;
-  }
-
-  private boolean artistInfoExists(String artistInfo){
-    boolean exists = false;
-    if (artistInfo != null) {
-      exists = true;
-    }
-    return exists;
-  }
-
-  private String markArtistAsLocallyStored(String artistInfo){
-    artistInfo = "[*]" + artistInfo;
-    return artistInfo;
-  }
-
-  private Response<String> getApiResponse(NYTimesAPI nyTimesApi, String artistName){
-    Response<String> callResponse = null;
-    try {
-      callResponse = nyTimesApi.getArtistInfo(artistName).execute();
-    } catch (IOException e1) {
-      Log.e("TAG", "Error " + e1);
-      e1.printStackTrace();
-    }
-    return callResponse;
-  }
-
-  private JsonObject apiResponseToJsonObject(Response<String> apiCallResponse){
-    Gson gson = new Gson();
-    JsonObject jsonObject = gson.fromJson(apiCallResponse.body(), JsonObject.class);
-    JsonObject jsonObjectResponse = jsonObject.get("response").getAsJsonObject();
-    return jsonObjectResponse;
-  }
-
-  private void setOpenUrlButtonListener(JsonElement jsonUrl){
-    final String urlInFinalString = jsonUrl.getAsString();
-    findViewById(R.id.openUrlButton).setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        Intent intent = new Intent(Intent.ACTION_VIEW);
-        intent.setData(Uri.parse(urlInFinalString));
-        startActivity(intent);
-      }
-    });
-  }
-
-  private void setNYTimesImageIntoView(){
-    String imageUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVioI832nuYIXqzySD8cOXRZEcdlAj3KfxA62UEC4FhrHVe0f7oZXp3_mSFG7nIcUKhg&usqp=CAU";
-    android.view.View view = findViewById(R.id.imageView);
-
-    Log.e("TAG","Get Image from " + imageUrl);
-
-    runOnUiThread( () -> Picasso.get().load(imageUrl).into((ImageView) view) );
-  }
-
-  private void setArtistInfoIntoView(String artistInfo){
-    final String finalArtistInfo = artistInfo;
-    runOnUiThread( () -> ArtistInfoView.setText(Html.fromHtml(finalArtistInfo)) );
-  }
-
-  private JsonElement getDocumentAbstract(JsonObject apiCallResponseInJson){
-    JsonElement documentAbstract = apiCallResponseInJson.get("docs").getAsJsonArray().get(0).getAsJsonObject().get("abstract");
-    return documentAbstract;
-  }
-
-  private JsonElement getDocumentUrl(JsonObject apiCallResponseInJson){
-    JsonElement url = apiCallResponseInJson.get("docs").getAsJsonArray().get(0).getAsJsonObject().get("web_url");
-    return url;
-  }
-
-  private String formatAbstractArtistInfo(JsonElement documentAbstractArtistInfo, String artistName){
-    String formattedArtistInfo;
-
-    if (documentAbstractArtistInfo == null){
-      formattedArtistInfo = "No Results";
-    }
-    else {
-      formattedArtistInfo = documentAbstractArtistInfo.getAsString().replace("\\n", "\n");
-      formattedArtistInfo = textToHtml(formattedArtistInfo, artistName);
-      DataBase.saveArtist(dataBase, artistName, formattedArtistInfo);
-    }
-    return formattedArtistInfo;
-  }
-
-  private String getArtistInfoFromService(NYTimesAPI nyTimesApi, String artistName){
-    Response<String> nyTimesApiResponse = getApiResponse(nyTimesApi, artistName);
-
-    String formattedArtistInfo = null;
-
-    if (nyTimesApiResponse != null){ // si se obtuvo respuesta
-      Log.e("TAG","JSON " + nyTimesApiResponse.body());
-      JsonObject responseInJson = apiResponseToJsonObject(nyTimesApiResponse);
-
-      JsonElement documentAbstractArtistInfo = getDocumentAbstract(responseInJson);
-      formattedArtistInfo = formatAbstractArtistInfo(documentAbstractArtistInfo, artistName);
-
-      JsonElement documentUrl = getDocumentUrl(responseInJson);
-      setOpenUrlButtonListener(documentUrl);
+    private fun createNYTimesApi(): NYTimesAPI {
+        val retrofit = retrofit
+        return retrofit.create(NYTimesAPI::class.java)
     }
 
-    return formattedArtistInfo;
-  }
-
-  private Thread createThread(NYTimesAPI nyTimesApi, String artistName){
-    Thread thread = new Thread(new Runnable() {
-      @Override
-      public void run() {
-        String artistInfo = DataBase.getInfo(dataBase, artistName);
-
-        if (artistInfoExists(artistInfo)) {
-          artistInfo = markArtistAsLocallyStored(artistInfo);
+    private fun artistInfoExists(artistInfo: String?): Boolean {
+        var exists = false
+        if (artistInfo != null) {
+            exists = true
         }
-        else {
-          artistInfo = getArtistInfoFromService(nyTimesApi, artistName);
+        return exists
+    }
+
+    private fun markArtistAsLocallyStored(artistInfo: String?): String {
+        var artistInfo = artistInfo
+        artistInfo = "[*]$artistInfo"
+        return artistInfo
+    }
+
+    private fun getApiResponse(nyTimesApi: NYTimesAPI, artistName: String?): Response<String>? {
+        var callResponse: Response<String>? = null
+        try {
+            callResponse = nyTimesApi.getArtistInfo(artistName).execute()
+        } catch (e1: IOException) {
+            Log.e("TAG", "Error $e1")
+            e1.printStackTrace()
         }
+        return callResponse
+    }
 
-        setArtistInfoIntoView(artistInfo);
-        setNYTimesImageIntoView();
-      }
-    });
-    return thread;
-  }
+    private fun apiResponseToJsonObject(apiCallResponse: Response<String>): JsonObject {
+        val gson = Gson()
+        val jsonObject =
+            gson.fromJson(apiCallResponse.body(), JsonObject::class.java)
+        return jsonObject["response"].asJsonObject
+    }
 
-  public void getArtistInfo(String artistName) {
-    NYTimesAPI nyTimesApi = createNYTimesApi();
+    private fun setOpenUrlButtonListener(jsonUrl: JsonElement) {
+        val urlInFinalString = jsonUrl.asString
+        findViewById<View>(R.id.openUrlButton).setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse(urlInFinalString)
+            startActivity(intent)
+        }
+    }
 
-    Log.e("TAG","artistName " + artistName);
+    private fun setNYTimesImageIntoView() {
+        val imageUrl =
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVioI832nuYIXqzySD8cOXRZEcdlAj3KfxA62UEC4FhrHVe0f7oZXp3_mSFG7nIcUKhg&usqp=CAU"
+        val view = findViewById<View>(R.id.imageView)
+        Log.e("TAG", "Get Image from $imageUrl")
+        runOnUiThread { Picasso.get().load(imageUrl).into(view as ImageView) }
+    }
 
-    Thread thread = createThread(nyTimesApi, artistName);
-    thread.start();
-  }
+    private fun setArtistInfoIntoView(artistInfo: String?) {
+        runOnUiThread { ArtistInfoView!!.text = Html.fromHtml(artistInfo) }
+    }
 
-  private void open(String artist) {
-    dataBase = new DataBase(this);
-    getArtistInfo(artist);
-  }
+    private fun getDocumentAbstract(apiCallResponseInJson: JsonObject): JsonElement {
+        return apiCallResponseInJson["docs"].asJsonArray[0].asJsonObject["abstract"]
+    }
 
-  public static String textToHtml(String text, String term) {
-    StringBuilder builder = new StringBuilder();
+    private fun getDocumentUrl(apiCallResponseInJson: JsonObject): JsonElement {
+        return apiCallResponseInJson["docs"].asJsonArray[0].asJsonObject["web_url"]
+    }
 
-    builder.append("<html><div width=400> <font face=\"arial\">");
+    private fun formatAbstractArtistInfo(
+        documentAbstractArtistInfo: JsonElement?,
+        artistName: String?
+    ): String {
+        var formattedArtistInfo: String
+        if (documentAbstractArtistInfo == null) {
+            formattedArtistInfo = "No Results"
+        } else {
+            formattedArtistInfo = documentAbstractArtistInfo.asString.replace("\\n", "\n")
+            formattedArtistInfo = textToHtml(formattedArtistInfo, artistName)
+            DataBase.saveArtist(dataBase, artistName, formattedArtistInfo)
+        }
+        return formattedArtistInfo
+    }
 
-    String textWithBoldInHtml = text
-            .replace("'", " ")
-            .replace("\n", "<br>")
-            .replaceAll("(?i)" + term, "<b>" + term.toUpperCase() + "</b>");
+    private fun getArtistInfoFromService(nyTimesApi: NYTimesAPI, artistName: String?): String? {
+        val nyTimesApiResponse = getApiResponse(nyTimesApi, artistName)
+        var formattedArtistInfo: String? = null
+        if (nyTimesApiResponse != null) { // si se obtuvo respuesta
+            Log.e("TAG", "JSON " + nyTimesApiResponse.body())
+            val responseInJson = apiResponseToJsonObject(nyTimesApiResponse)
+            val documentAbstractArtistInfo = getDocumentAbstract(responseInJson)
+            formattedArtistInfo = formatAbstractArtistInfo(documentAbstractArtistInfo, artistName)
+            val documentUrl = getDocumentUrl(responseInJson)
+            setOpenUrlButtonListener(documentUrl)
+        }
+        return formattedArtistInfo
+    }
 
-    builder.append(textWithBoldInHtml);
-    builder.append("</font></div></html>");
-    return builder.toString();
-  }
+    private fun createThread(
+        nyTimesApi: NYTimesAPI,
+        artistName: String?
+    ): Thread {
+        return Thread {
+            var artistInfo = DataBase.getInfo(dataBase, artistName)
+            artistInfo = if (artistInfoExists(artistInfo)) {
+                markArtistAsLocallyStored(artistInfo)
+            } else {
+                getArtistInfoFromService(nyTimesApi, artistName)
+            }
+            setArtistInfoIntoView(artistInfo)
+            setNYTimesImageIntoView()
+        }
+    }
+
+    fun getArtistInfo(artistName: String?) {
+        val nyTimesApi = createNYTimesApi()
+        Log.e("TAG", "artistName $artistName")
+        val thread = createThread(nyTimesApi, artistName)
+        thread.start()
+    }
+
+    private fun open(artist: String?) {
+        dataBase = DataBase(this)
+        getArtistInfo(artist)
+    }
+
+    companion object {
+        const val ARTIST_NAME_EXTRA = "artistName"
+        fun textToHtml(text: String, term: String?): String {
+            val builder = StringBuilder()
+            builder.append("<html><div width=400> <font face=\"arial\">")
+            val textWithBoldInHtml = text
+                .replace("'", " ")
+                .replace("\n", "<br>")
+                .replace(
+                    "(?i)$term".toRegex(),
+                    "<b>" + term!!.uppercase(Locale.getDefault()) + "</b>"
+                )
+            builder.append(textWithBoldInHtml)
+            builder.append("</font></div></html>")
+            return builder.toString()
+        }
+    }
 }
