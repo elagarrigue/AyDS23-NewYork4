251be613c77b2c98ccb2a2054cffda8fbc82b663
Zeke <erb1995@gmail.com>
2023-05-16 11:46:38 -0300

    Update SongDescriptionHelperTest.kt
    - Fixed declarations.
    - Added missing tests.
    - Fixed existing tests.
    - Run tests, they all work fine.

--- app/src/test/java/ayds/newyork/songinfo/home/view/SongDescriptionHelperTest.kt
+++ app/src/test/java/ayds/newyork/songinfo/home/view/SongDescriptionHelperTest.kt
@@ -8,64 +8,192 @@ import org.junit.Test
 
 class SongDescriptionHelperTest {
 
-    private val songDescriptionHelper by lazy { SongDescriptionHelperImpl() }
+    private val strategyFactory: SpotifySongReleaseDateStrategyFactory = SpotifySongReleaseDateStrategyFactory()
+    private val songDescriptionHelper by lazy {
+        SongDescriptionHelperImpl(strategyFactory)
+    }
 
     @Test
-    fun `given a local song it should return the description`() {
+    fun `given a local song it should return the description with day precision`() {
         val song: Song = SpotifySong(
             "id",
             "Plush",
             "Stone Temple Pilots",
             "Core",
             "1992-01-01",
+            "day",
             "url",
-            "url",
+            "imageUrl",
             true,
         )
-
         val result = songDescriptionHelper.getSongDescriptionText(song)
-
         val expected =
             "Song: Plush [*]\n" +
-                    "Artist: Stone Temple Pilots\n" +
-                    "Album: Core\n" +
-                    "Year: 1992"
-
+            "Artist: Stone Temple Pilots\n" +
+            "Album: Core\n" +
+            "Release date: 01/01/1992\n"
         Assert.assertEquals(expected, result)
     }
 
     @Test
-    fun `given a non local song it should return the description`() {
+    fun `given a local song it should return the description with month precision`() {
+        val song: Song = SpotifySong(
+            "id",
+            "Plush",
+            "Stone Temple Pilots",
+            "Core",
+            "1992-01",
+            "month",
+            "url",
+            "imageUrl",
+            true,
+        )
+        val result = songDescriptionHelper.getSongDescriptionText(song)
+        val expected =
+            "Song: Plush [*]\n" +
+            "Artist: Stone Temple Pilots\n" +
+            "Album: Core\n" +
+            "Release date: January, 1992\n"
+        Assert.assertEquals(expected, result)
+    }
+
+    @Test
+    fun `given a local song it should return the description with year precision (leap)`() {
+        val song: Song = SpotifySong(
+            "id",
+            "Plush",
+            "Stone Temple Pilots",
+            "Core",
+            "1992",
+            "year",
+            "url",
+            "imageUrl",
+            true,
+        )
+        val result = songDescriptionHelper.getSongDescriptionText(song)
+        val expected =
+            "Song: Plush [*]\n" +
+            "Artist: Stone Temple Pilots\n" +
+            "Album: Core\n" +
+            "Release date: 1992 (leap year)\n"
+        Assert.assertEquals(expected, result)
+    }
+
+    @Test
+    fun `given a local song it should return the description with year precision (not leap)`() {
+        val song: Song = SpotifySong(
+            "id",
+            "Plush",
+            "Stone Temple Pilots",
+            "Core",
+            "1991",
+            "year",
+            "url",
+            "imageUrl",
+            true,
+        )
+        val result = songDescriptionHelper.getSongDescriptionText(song)
+        val expected =
+            "Song: Plush [*]\n" +
+            "Artist: Stone Temple Pilots\n" +
+            "Album: Core\n" +
+            "Release date: 1991 (not a leap year)\n"
+        Assert.assertEquals(expected, result)
+    }
+
+    @Test
+    fun `given a non local song it should return the description with day precision`() {
         val song: Song = SpotifySong(
             "id",
             "Plush",
             "Stone Temple Pilots",
             "Core",
             "1992-01-01",
+            "day",
             "url",
-            "url",
+            "imageUrl",
             false,
         )
-
         val result = songDescriptionHelper.getSongDescriptionText(song)
-
         val expected =
             "Song: Plush \n" +
-                    "Artist: Stone Temple Pilots\n" +
-                    "Album: Core\n" +
-                    "Year: 1992"
+            "Artist: Stone Temple Pilots\n" +
+            "Album: Core\n" +
+            "Release date: 01/01/1992\n"
+        Assert.assertEquals(expected, result)
+    }
 
+    @Test
+    fun `given a non local song it should return the description with month precision`() {
+        val song: Song = SpotifySong(
+            "id",
+            "Plush",
+            "Stone Temple Pilots",
+            "Core",
+            "1992-01",
+            "month",
+            "url",
+            "imageUrl",
+            false,
+        )
+        val result = songDescriptionHelper.getSongDescriptionText(song)
+        val expected =
+            "Song: Plush \n" +
+            "Artist: Stone Temple Pilots\n" +
+            "Album: Core\n" +
+            "Release date: January, 1992\n"
+        Assert.assertEquals(expected, result)
+    }
+
+    @Test
+    fun `given a non local song it should return the description with year precision (leap)`() {
+        val song: Song = SpotifySong(
+            "id",
+            "Plush",
+            "Stone Temple Pilots",
+            "Core",
+            "1992",
+            "year",
+            "url",
+            "imageUrl",
+            false,
+        )
+        val result = songDescriptionHelper.getSongDescriptionText(song)
+        val expected =
+            "Song: Plush \n" +
+            "Artist: Stone Temple Pilots\n" +
+            "Album: Core\n" +
+            "Release date: 1992 (leap year)\n"
+        Assert.assertEquals(expected, result)
+    }
+
+    @Test
+    fun `given a non local song it should return the description with year precision (not leap)`() {
+        val song: Song = SpotifySong(
+            "id",
+            "Plush",
+            "Stone Temple Pilots",
+            "Core",
+            "1991",
+            "year",
+            "url",
+            "imageUrl",
+            false,
+        )
+        val result = songDescriptionHelper.getSongDescriptionText(song)
+        val expected =
+            "Song: Plush \n" +
+            "Artist: Stone Temple Pilots\n" +
+            "Album: Core\n" +
+            "Release date: 1991 (not a leap year)\n"
         Assert.assertEquals(expected, result)
     }
 
     @Test
     fun `given a non spotify song it should return the song not found description`() {
         val song: Song = mockk()
-
         val result = songDescriptionHelper.getSongDescriptionText(song)
-
         val expected = "Song not found"
-
         Assert.assertEquals(expected, result)
     }
 }
