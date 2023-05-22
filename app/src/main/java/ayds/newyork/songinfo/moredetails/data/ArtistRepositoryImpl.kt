e634c7a3f2730916d46ec611ca0432a0a345c282
albanomazzino <albimazzino@gmail.com>
2023-05-22 19:47:09 -0300

    Removed 'external' package, now using nytimesdata service

--- app/src/main/java/ayds/newyork/songinfo/moredetails/data/ArtistRepositoryImpl.kt
+++ app/src/main/java/ayds/newyork/songinfo/moredetails/data/ArtistRepositoryImpl.kt
@@ -1,11 +1,12 @@
 package ayds.newyork.songinfo.moredetails.data
 
-import ayds.newyork.songinfo.moredetails.data.external.nytimes.NYTimesArtistService
+
 import ayds.newyork.songinfo.moredetails.data.local.nytimes.NYTimesLocalStorage
 import ayds.newyork.songinfo.moredetails.domain.entities.Artist
 import ayds.newyork.songinfo.moredetails.domain.entities.Artist.NYTimesArtist
 import ayds.newyork.songinfo.moredetails.domain.entities.Artist.EmptyArtist
 import ayds.newyork.songinfo.moredetails.domain.repository.ArtistRepository
+import com.test.artist.external.NYTimesArtistService
 
 internal class ArtistRepositoryImpl(
     private val nyTimesLocalStorage: NYTimesLocalStorage,
